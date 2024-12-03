18d4699f4f01e56a00e124309890d42a5a940777
MadhuChandra2507 <madhuchandra9425@gmail.com>
2024-12-03 10:43:20 +0530

    Create device.tf

---
+++ device.tf
@@ -0,0 +1,25 @@
+resource "logicmonitor_device" "my_device" {
+  auto_balanced_collector_group_id = 0
+  current_collector_id = 1
+  custom_properties = [
+        {
+            name = "addr"
+              value = "127.0.0.1"
+        },
+        {
+            name = "host"
+              value = "localhost"
+        }
+    ]
+  description = "This is a Cisco Router"
+  device_type = 0
+  disable_alerting = true
+  display_name = "Cisco Router"
+  enable_netflow = true
+  host_group_ids = "16,4,3"
+  link = "www.ciscorouter.com"
+  name = "collector.localhost"
+  netflow_collector_id = 1
+  preferred_collector_id = 2
+  related_device_id = -1
+}
