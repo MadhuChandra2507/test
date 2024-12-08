0394b424e62c507e47b7cf32535c5dad11e26307
MadhuChandra2507 <madhuchandra9425@gmail.com>
2024-12-08 21:34:08 +0530

    Update create.tf

--- create.tf
+++ create.tf
@@ -6,6 +6,22 @@ terraform {
     }
   }
 }
+variable "logicmonitor_api_id" {
+  description = "LogicMonitor API ID"
+  type        = string
+  default     = ""  
+}
+
+variable "logicmonitor_api_key" {
+  description = "LogicMonitor API Key"
+  type        = string
+  default     = "" 
+
+variable "logicmonitor_company" {
+  description = "LogicMonitor company subdomain"
+  type        = string
+  default     = ""  
+}
 provider "logicmonitor" {
   api_id = var.logicmonitor_api_id
   api_key = var.logicmonitor_api_key
